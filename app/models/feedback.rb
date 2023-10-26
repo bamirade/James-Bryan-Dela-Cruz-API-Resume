fe248b9ca6310a110e09cc0d2d2984e9fd5534a1
bamirade <118993260+bamirade@users.noreply.github.com>
2023-10-27 01:31:21 +0800

    Model validator render fix

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,4 @@
 class Feedback < ApplicationRecord
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  validates :message, present: true, length: { maximum: 600, minimum: 6 }
+  validates :message, presence: true, length: { maximum: 600, minimum: 6 }
 end
